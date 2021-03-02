ab623c34ce666e781af70dd27ef8ac1e953875c8
ArinaMlt <79594454+ArinaMlt@users.noreply.github.com>
2021-03-02 15:51:22 +0800

    Add files via upload

---
+++ One.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 5 == 0) && (i % 3 == 0)) {
+                System.out.println("FizzBuzz");
+            } else {
+                if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                }
+                if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+                System.out.println(i);
+            }
+        }
+    }
+}
